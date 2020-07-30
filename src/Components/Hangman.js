ddec4467b34f9c2f06217f405e185acd0b24eedf
Riyiike <odusanyaiyanuoluwa@gmail.com>
2020-07-30 00:05:11 -0800

    finish up

--- src/Components/Hangman.js
+++ src/Components/Hangman.js
@@ -23,14 +23,70 @@ constructor(props) {
     this.state = {
         mistake : 0,
         guessed : new Set ([]),
-        answer :  randomWord();
+        answer :  randomWord()
     }    
 }
 
+handleGuess = e => {
+    let letter = e.target.value;
+    this.setState(st => ({
+        guessed: st.guessed.add(letter),
+        mistake: st.mistake + (st.answer.includes(letter)? 0 : 1 )
+    }));
+}
+guessedWord(){
+    return this.state.answer.split('').map(letter =>(this.state.guessed.has(letter)? letter:" _ "));
+}
+
+generateButtons(){
+    return "abcdefghijklmnopqrstuvwxyz".split("").map(letter => (
+        <button 
+        className = "btn btn-lg btn-primary m-2"
+        key={letter}
+        value={letter}
+        onClick ={this.handleGuess}
+        disabled={this.state.guessed.has(letter)}
+        >
+            {letter}
+        </button>
+    ));
+}
+resetButton = () => {
+    this.setState({
+        mistake:0,
+        guessed:new Set([]),
+        answer:randomWord()
+
+    });
+    
+}
+
 render (){
+    const gameOver = this.state.mistake >= this.props.maxWrong;
+    const isWinner = this.guessedWord().join("") === this.state.answer;
+    let gameStat = this.generateButtons();
+
+    if(isWinner){
+        gameStat ="You Won!!!"
+    }
+
+    if (gameOver){
+        gameStat ="You lost!!!"
+    }
+
     return (
-        <div>
-        <h1>Hangman</h1>
+        <div className ="Hangman container">
+        <h1 className = "text-center">Hangman</h1>
+        <div className ="float-right">Wrong Gueeses : {this.state.mistake} of {this.props.maxWrong}</div>
+        <div className="text-center">
+        <img src ={this.props.images[this.state.mistake]} alt =''/>
+        </div>
+        <div className ="text-center">
+        <p>Guess the Programming Language:</p>
+        <p>{!gameOver ? this.guessedWord() : this.state.answer}</p>
+        <p>{gameStat}</p>
+        <button className="btn btn-info" onClick={this.resetButton}>Reset</button>
+        </div>
         </div>
     )
 }
--- src/Components/Words.js
+++ src/Components/Words.js
@@ -1,2 +1,23 @@
-import React from 'react'
-import 'Hangman' from './Components/Hangman';
+import React from 'react';
+// import Hangman from './Components/Hangman';
+
+let programming_languages =[
+    "Python",
+    "Javascript",
+    "Mongodb",
+    "json",
+    "java",
+    "html",
+    "css",
+    "c",
+    "csharp",
+    "golang",
+    "kotlin",
+    "ruby"
+]
+
+function randomWord (){
+    return programming_languages [Math.floor (Math.random () * programming_languages.length)]
+}
+export {randomWord};
+
